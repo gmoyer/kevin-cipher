e1df6eff65824703a307cf20fde9a9391fd420fa
Griffin Moyer <moyergriffin@gmail.com>
2022-10-12 11:45:31 -0600

    type in middle of box work

--- livetype/index.js
+++ livetype/index.js
@@ -1,38 +1,52 @@
 const LETTER = "abcdefghijklmnopqrstuvwxyz"; //for getting correct letter position
-const PUNCTUATION = " .,!?";
-
+const PUNCTUATION = " .,!?:;0123456789";
 
 var base = "m";
 var bswap = false;
+var textlength = 0;
 
 $( document ).ready(function() {
     $('#inBox').bind('input propertychange', function() {
-        var intxt = $('#inBox').val().toLowerCase().slice(-1);
-        //cycle through all the letters in the input text
-        if (LETTER.indexOf(intxt) == -1) {
-            if (PUNCTUATION.indexOf(intxt) != -1) {
-                $("#inBox").append(intxt);
-            } else {
-                $("#inBox").val($('#inBox').val().slice(0, -1));
-            }
-            if (intxt == "~") {
-                bswap = true;
-            }
+        var currentlength = $('#inBox').val().length;
+        if (currentlength == 1 + textlength) {
+            //------------------Single character added-------------------
+            var loc = document.getElementById('inBox').selectionStart;
+            console.log(loc);
+            var intxt = $('#inBox').val().toLowerCase().slice(loc-1, loc);
+            //cycle through all the letters in the input text
+            if (LETTER.indexOf(intxt) == -1) {
+                if (PUNCTUATION.indexOf(intxt) != -1) {
+                    $("#inBox").append(intxt);
+                } else {
+                    $("#inBox").val($('#inBox').val().slice(0, loc-1)+$('#inBox').val().slice(loc));
+                }
+                if (intxt == "~") {
+                    if (bswap) {
+                        bswap = false;
+                    } else {
+                        bswap = true;
+                    }
+                }
 
-        }
-        else {
-            var unicode = 3400; 
-            if (bswap) {
-                base = intxt;
-                unicode += 27;
-                bswap = false;
-            } else {
-                unicode += LETTER.indexOf(intxt) + 1; //getting correct letter
             }
-            unicode += LETTER.indexOf(base) * 27; //setting correct base
-            var unistring = "0x" + unicode;
-            console.log(unistring);
-            $("#inBox").val($('#inBox').val().slice(0, -1) + String.fromCodePoint(unistring));
+            else {
+                var unicode = 3400; 
+                if (bswap) {
+                    unicode += 27;
+                    bswap = false;
+                } else {
+                    //find the correct base
+                    if (textlength != 0) {
+                        var prevChar = $("#inBox").val().slice(loc-2, loc-1);
+                        //console.log(prevChar.charCodeAt());
+                    }
+                    unicode += LETTER.indexOf(intxt) + 1; //getting correct letter
+                }
+                unicode += LETTER.indexOf(base) * 27; //setting correct base
+                var unistring = "0x" + unicode;
+                $("#inBox").val($('#inBox').val().slice(0, loc-1)+String.fromCodePoint(unistring)+$('#inBox').val().slice(loc));
+                textlength += 1;
+            }
         }
         //$("#inBox").html(outtxt);
     });
